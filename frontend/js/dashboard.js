3c04b594a9ed9a1186ce03fe528da9d49d2ca0e0
Aiswarya-s2413 <aiswaryaakku9@gmail.com>
2025-08-12 14:08:07 +0530

    added vercel.json

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,6 +1,6 @@
 function logout(event) {
     if (event) event.preventDefault();
-    fetch('https://3.27.123.53.sslip.io/api/logout/', {
+    fetch('/api/logout/', {
         method: 'POST',
         credentials: 'include' 
     })
